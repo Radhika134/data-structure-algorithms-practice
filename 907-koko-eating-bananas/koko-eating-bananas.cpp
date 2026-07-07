b4172a437b14f3998fe792aece54de907dbf1ad5
Radhika134 <radhikaagg134@gmail.com>
2026-07-07 17:29:04 +0530

    Time: 18 ms (10.76%) | Memory: 22.9 MB (47.49%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
-    int findMax(vector<int> &piles,int n)
-    {
+    int findMax(vector<int>& piles, int n) {
         int maxi = INT_MIN;
-        for(int i=0;i<n;i++)
-        {
+        for (int i = 0; i < n; i++) {
             maxi = max(maxi, piles[i]);
         }
         return maxi;
@@ -22,21 +20,17 @@ public:
         int n = piles.size();
         int ans = -1;
 
-        int high = findMax(piles,n), low = 1;
+        int high = findMax(piles, n), low = 1;
 
         while (low <= high) {
-            int mid = (low + high) / 2;
-            long long hrs = timeTaken(piles,mid);
-            if (hrs<=h)
-            {
+            int mid = low + ((high - low) / 2);
+            long long hrs = timeTaken(piles, mid);
+            if (hrs <= h) {
                 ans = mid;
-                high = mid-1;
-            } 
-            else if (hrs > h) 
-            {
+                high = mid - 1;
+            } else if (hrs > h) {
                 low = mid + 1;
             }
-          
         }
         return ans;
     }
